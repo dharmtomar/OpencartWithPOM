d2343baebd06c6116f874c2a3df862e1e3df608f
dharmtomar <dtdtomar2@gmail.com>
2024-04-22 18:49:55 +0530

    e2e class add in addtocart branch

---
+++ src/main/java/com/qa/opencart/pages/E2ETest.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class E2ETest {
+WebDriver driver;
+	public E2ETest(WebDriver driver) {
+		this.driver=driver;
+	}
+}
